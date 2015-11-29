3808fb518fa70be68c0b60218fde4994c56bb3d6
Caio Carrara <ccarrara@thoughtworks.com>
2015-11-29 17:38:42 -0200

    Changes the virtualenv creation process
    The previous process only created the application virtual environment if
    the virtualenv wasn't installed. This behaviour could cause an
    unexpected result if (for any reason) the virtualenv program was already
    installed and the virtual enviroment wasn't created.
    
    This change changes this process to always create the virtual
    environment, independently of virtualenv creation. It also changes the
    virtualenv target directory to use a more appropriate location (same
    default location used by virtualenvwrapper).

--- provisioning/python.sh
+++ provisioning/python.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+VIRTUALENVS_HOME=/home/vagrant/.virtualenvs
+VIRTUALENV_GAC=${VIRTUALENVS_HOME}/GAC
+
 is_pip_package_installed() {
     PIP3_SHOW_OUTPUT=$(pip3 show "$1" | grep "$1")
     if [ "" = "$PIP3_SHOW_OUTPUT" ]; then
@@ -17,6 +20,11 @@ if is_pip_package_installed virtualenv; then
     echo "Virtualenv already installed"
 else
     sudo pip3 install virtualenv
-    virtualenv GAC
-    echo "source ~/GAC/bin/activate" | tee -a /home/vagrant/.bashrc
 fi
+
+# p option makes no error be reported if directory already exists
+mkdir -p $VIRTUALENVS_HOME
+virtualenv $VIRTUALENV_GAC
+chown -R vagrant:vagrant $VIRTUALENVS_HOME
+
+echo "source ${VIRTUALENV_GAC}/bin/activate" | tee -a /home/vagrant/.bashrc
