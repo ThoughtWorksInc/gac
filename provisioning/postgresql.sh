fd6593fac3d386a83c3f14d51157c8b8321bd956
Denis Costa <deniscostadsc@gmail.com>
2015-12-15 16:50:59 -0200

    Adiciona psycopg2 e suas dependências.

--- provisioning/postgresql.sh
+++ provisioning/postgresql.sh
@@ -8,6 +8,7 @@ else
     sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | tee -a /etc/apt/sources.list.d/pgdg.list
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     sudo apt-get -y update
+    install_package_if_not_present postgresql-server-dev-all
     install_package_if_not_present postgresql-9.4
 fi
 
